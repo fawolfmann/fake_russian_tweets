f1fa85fdff47004850f026c1e762ed10e98e7049
fabian40 <fabian@muttdata.ai>
2021-08-01 21:47:41 -0300

    add placeholder for shape

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 """Api entrypoint"""
 from typing import Dict
 
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, Request
 from fastapi.responses import HTMLResponse
 from pydantic import BaseModel  # pylint-disable=no-name-in-module
 
@@ -24,20 +24,18 @@ class TweetResponse(BaseModel):
 @app.post("/predict", response_model=TweetResponse)
 def predict(request: TweetRequest, model: FakeTweetsModel = Depends(load_model)):
     response = model.predict(request.text)
-    if response:
-        sentiment, confidence, probabilities = response
-        return TweetResponse(
-            sentiment=sentiment, confidence=confidence, probabilities=probabilities
-        )
-    else:
-        html_content = """
-            <html>
-                <head>
-                    <title>Some HTML in here</title>
-                </head>
-                <body>
-                    <h1>Look ma! HTML!</h1>
-                </body>
-            </html>
-            """
-    return HTMLResponse(content=html_content, status_code=400)
+    sentiment, confidence, probabilities = response
+    return TweetResponse(
+        sentiment=sentiment, confidence=confidence, probabilities=probabilities
+    )
+
+
+@app.get("/{text}", response_class=HTMLResponse)
+def bert_explained(
+    request: Request, text: str, model: FakeTweetsModel = Depends(load_model)
+):
+    return HTMLResponse(shape_explained(text))
+
+
+def shape_explained(text: str):
+    return text
